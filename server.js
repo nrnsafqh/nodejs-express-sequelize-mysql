76718ac996ae35d45643fb090ed96aae906ed5f5
Anis <nuranisafiqah2105@gmail.com>
2021-01-21 11:12:01 +0800

    21/1

--- server.js
+++ server.js
@@ -21,6 +21,8 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome to bezkoder application." });
 });
 
+require("./app/routes/tutorial.routes")(app);
+
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
